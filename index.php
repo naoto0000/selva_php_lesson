b3f2b70f25cc827d8a754ac1373c6e433538a038
naoto0000 <fukuoka0583@gmail.com>
2024-02-02 14:21:56 +0900

    php課題 2 完了

---
+++ index.php
@@ -0,0 +1,80 @@
+<?php
+require_once("function.php");
+
+if (isset($_POST['login_submit'])) {
+    $login_mail = $_SESSION['login_id'] = $_POST['login_id'];
+    $login_pass = $_SESSION['login_pass'] = $_POST['login_pass'];
+
+    $login_sql = "SELECT * FROM members WHERE email = :email";
+    $login_stmt = $pdo->prepare($login_sql);
+    $login_stmt->bindValue(':email', $login_mail, PDO::PARAM_STR);
+    $login_stmt->execute();
+    $login_result = $login_stmt->fetch();
+
+    if ($login_mail == "" || $login_pass == "" || $login_mail == "" && $login_pass == "") {
+        $_SESSION['login_error'] = true;
+        header('Location: login.php');
+        exit();
+    }
+
+    // パスワードのチェック
+    if (password_verify($login_pass, $login_result['password'])) {
+
+        //DBのユーザー情報をセッションに保存
+        $_SESSION['name'] = $login_result['name_sei'] . $login_result['name_mei'];
+
+        $_SESSION['login'] = 1;
+
+        $_SESSION['login_id'] = "";
+        $login_id = "";
+    } else {
+        $_SESSION['login_error'] = true;
+        header('Location: login.php');
+        exit();
+    }
+} else {
+    $_SESSION['login'] = "";
+}
+
+
+if (isset($_POST['logout'])) {
+    $_SESSION['login'] = "";
+}
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>福岡直斗　課題</title>
+</head>
+
+<body>
+    <header>
+        <div class="header_name">
+            <?php if ($_SESSION['login'] == 1) : ?>
+                <p>ようこそ<?php echo $_SESSION['name']; ?>様</p>
+            <?php endif; ?>
+
+        </div>
+        <div class="header_link">
+            <form action="" method="post">
+                <?php if ($_SESSION['login'] == 1) : ?>
+                    <input type="submit" name="logout" class="btn logout_btn" value="ログアウト">
+                <?php else : ?>
+                    <div class="header_login_group">
+                        <button type="button" onclick="location.href='member_regist.php'" class="btn">新規会員登録</button>
+                        <button type="button" onclick="location.href='login.php'" class="btn">ログイン</button>
+                    </div>
+                <?php endif; ?>
+            </form>
+        </div>
+    </header>
+</body>
+
+</html>
